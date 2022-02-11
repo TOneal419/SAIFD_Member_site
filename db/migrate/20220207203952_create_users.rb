548d5f71ceba82c9c977bfa891e7ded5f34b0048
isaacy13 <iyastros@gmail.com>
2022-02-11 11:58:12 -0600

    default admin acc implemented

--- db/migrate/20220207203952_create_users.rb
+++ db/migrate/20220207203952_create_users.rb
@@ -16,6 +16,9 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       CREATE SEQUENCE users_user_id_seq START 1;
       ALTER SEQUENCE users_user_id_seq OWNED BY users.user_id;
       ALTER TABLE users ALTER COLUMN user_id SET DEFAULT nextval('users_user_id_seq');
+      
+      INSERT INTO users(email, first_name, last_name, class_year, role_id, created_at, updated_at)
+      VALUES ('wjmckinley@tamu.edu', 'Bill', 'McKinley', NULL, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
     SQL
   end
 
