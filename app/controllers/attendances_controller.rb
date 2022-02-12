1ef2dfcb238a859c916891c1f7b5849e1983c56c
TOneal419 <56747323+TOneal419@users.noreply.github.com>
2022-02-12 13:50:02 -0600

    Attempted to fix Rubocop

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,30 +3,30 @@
 # AttendancesController
 class AttendancesController < ApplicationController
     before_action :set_attendance, only: %i[ show edit update destroy ]
-  
+
     # GET /attendances or /attendances.json
     def index
       @attendances = Attendance.all
     end
-  
+
     # GET /attendances/1 or /attendances/1.json
     def show
     end
-  
+
     # GET /attendances/new
     def new
       @attendance = Attendance.new
     end
-  
+
     # GET /attendances/1/edit
     def edit
     end
-  
+
     # POST /attendances or /attendances.json
     def create
       @attendance = Attendance.new(attendance_params)
       # @attendance.update(user_id: ) # TODO: automatically set user_id here...
-  
+
       respond_to do |format|
         if @attendance.save
           format.html { redirect_to attendance_url(@attendance), notice: "Attendance was successfully created." }
@@ -37,40 +37,40 @@ class AttendancesController < ApplicationController
         end
       end
     end
-  end
+  
 
-  # PATCH/PUT /attendances/1 or /attendances/1.json
-  def update
-    respond_to do |format|
-      if @attendance.update(attendance_params)
-        format.html { redirect_to attendance_url(@attendance), notice: 'Attendance was successfully updated.' }
-        format.json { render :show, status: :ok, location: @attendance }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @attendance.errors, status: :unprocessable_entity }
-      end
+    # PATCH/PUT /attendances/1 or /attendances/1.json
+    def update
+        respond_to do |format|
+          if @attendance.update(attendance_params)
+            format.html { redirect_to attendance_url(@attendance), notice: 'Attendance was successfully updated.' }
+            format.json { render :show, status: :ok, location: @attendance }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @attendance.errors, status: :unprocessable_entity }
+          end
+        end
     end
-  end
 
-  # DELETE /attendances/1 or /attendances/1.json
-  def destroy
-    @attendance.destroy
+    # DELETE /attendances/1 or /attendances/1.json
+    def destroy
+        @attendance.destroy
 
-    respond_to do |format|
-      format.html { redirect_to attendances_url, notice: 'Attendance was successfully destroyed.' }
-      format.json { head :no_content }
+        respond_to do |format|
+          format.html { redirect_to attendances_url, notice: 'Attendance was successfully destroyed.' }
+          format.json { head :no_content }
+        end
     end
-  end
 
-  private
+    private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_attendance
-    @attendance = Attendance.find(params[:id])
-  end
+        # Use callbacks to share common setup or constraints between actions.
+        def set_attendance
+            @attendance = Attendance.find(params[:id])
+        end
 
-  # Only allow a list of trusted parameters through.
-  def attendance_params
-    params.require(:attendance).permit(:event_id, :user_id, :attend_time_start, :attend_time_end)
-  end
-end
+        # Only allow a list of trusted parameters through.
+        def attendance_params
+            params.require(:attendance).permit(:event_id, :user_id, :attend_time_start, :attend_time_end)
+        end
+    end
