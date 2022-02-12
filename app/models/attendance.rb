2ea0fde53b8367843fde8466423445542ce4be15
Zach Stepic <stepic.joseph@gmail.com>
2022-02-12 12:07:06 -0600

    model validation

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,2 +1,6 @@
 class Attendance < ApplicationRecord
+	validates :event_id, presence: true
+	validates :user_id, presence: true
+	validates :attend_time_start, presence: true
+	validates :attend_time_end, presence: true
 end
