ad221650ce29965aeaf2a43fac2d67dae7e867ec
isaacy13 <iyastros@gmail.com>
2022-03-25 14:48:10 -0500

    new entity forms are styled

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,5 +8,10 @@ class DashboardsController < ApplicationController
     @create_modify_events = @perms[:create_modify_events]
     @create_modify_announcements = @perms[:create_modify_announcements]
     @user = grab_user
+
+    # TODO: list out events and announcements
+    # @current_time = DateTime.now()
+    # @active_events = Event.where(event_time_start: , event_time_end: )
+    # @recent_announcements = Announcement.where()
   end
 end
