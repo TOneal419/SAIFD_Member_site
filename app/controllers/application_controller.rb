8c98f800a72ee9ac075d20b68293d50ab8627ba0
isaacy13 <iyastros@gmail.com>
2022-03-26 15:04:30 -0500

    edge case: invalid user session fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,14 @@ class ApplicationController < ActionController::Base
   # gets info of user currently logged in
   def grab_user
     @id_token = cookies[:current_user_session]
-    @email = Admin.where(uid: @id_token).first.email
-    @user = User.where(email: @email).first
+    @google_acc = Admin.where(uid: @id_token)
+    if @id_token.nil? || @google_acc.nil?
+      cookies[:current_user_session] = nil
+      return redirect_to '/', notice: 'Invalid user session.'
+    else
+      @email = @google_acc.first.email
+      @user = User.where(email: @email).first
+    end
   end
 
   def grab_permissions
