c1f4b695566a076fbd885f88bd261b415464a810
TOneal419 <tyler.michael.oneal@gmail.com>
2022-03-25 19:36:06 -0500

    Final Version, need to clean up emails and tables

--- app/mailers/usermailer_mailer.rb
+++ app/mailers/usermailer_mailer.rb
@@ -12,4 +12,5 @@ class UsermailerMailer < ApplicationMailer
       @announcement = announcement
       @url = 'www.heroku.com'
       mail(to: @user.email, subject: '[SAIFD Member Site] ' << @announcement.title)
+   end
 end
