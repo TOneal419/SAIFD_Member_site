0a20313d213350675f1fc78185dcd1329864be4a
isaacy13 <iyastros@gmail.com>
2022-04-01 19:38:00 -0500

    env vars removed, form width changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,11 +119,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '308710543738-t6dp4sal56ghupflaaud23spm9vqh53h.apps.googleusercontent.com'
-  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
-  ENV['mail_name'] = 'saifdmembertracking@gmail.com'
-  ENV['mail_password'] = 'zuwjngjfozckxbdb'
-  
+    
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
