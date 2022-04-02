74ed936798b5b0bf2f06cd35e21d743a0e67d7f3
isaacy13 <iyastros@gmail.com>
2022-04-01 20:21:22 -0500

    changed email url

--- app/mailers/usermailer_mailer.rb
+++ app/mailers/usermailer_mailer.rb
@@ -2,14 +2,14 @@
 class UsermailerMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url = 'www.heroku.com'
+    @url = 'https://saifd-tamu.herokuapp.com/'
     mail(to: @user.email, subject: '[SAIFD Member Site] Welcome to the SAIFD Member Site')
   end
 
   def announce_all(user, announcement)
     @user = user
     @announcement = announcement
-    @url = 'www.heroku.com'
+    @url = 'https://saifd-tamu.herokuapp.com/'
     mail(to: @user.email, subject: '[SAIFD Member Site] ' << @announcement.title)
   end
 end
